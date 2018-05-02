5d66055bac4248e37266b0c46beecb829001e8f5
JuliaPotyavina <Julia_Potyavina@mail.ru>
2018-05-02 20:15:00 +0300

    Модуляция

--- js/util.js
+++ js/util.js
@@ -1,59 +1,58 @@
 'use strict';
 
-(function() {
-	window.util = {
-		disableElements: function (elements) {
-		  for (var i = 0; i < elements.length; i++) {
-		    elements[i].disabled = true;
-		  }
-		},
+(function () {
+  window.util = {
+    disableElements: function (elements) {
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].disabled = true;
+      }
+    },
 
-		// Функция для случайного числа (включая max)
-		getRandomNumber: function (min, max) {
-		  return Math.floor(min + Math.random() * (max - min + 1));
-		},
+    // Функция для случайного числа (включая max)
+    getRandomNumber: function (min, max) {
+      return Math.floor(min + Math.random() * (max - min + 1));
+    },
 
-		// Случайная сортировка массивов
-		sortRandomArray: function (arr) {
-		  function compareRandom() {
-		    return Math.random() - 0.5;
-		  }
+    // Случайная сортировка массивов
+    sortRandomArray: function (arr) {
+      function compareRandom() {
+        return Math.random() - 0.5;
+      }
+      return arr.sort(compareRandom);
+    },
 
-		  return arr.sort(compareRandom);
-		},
+    // Функция для рандомного индекса
+    getRandomIndex: function (arr) {
+      var index = window.util.getRandomNumber(0, arr.length);
+      return index;
+    },
 
-		// Функция для рандомного индекса
-		getRandomIndex: function (arr) {
-		  var index = window.util.getRandomNumber(0, arr.length);
-		  return index;
-		},
+    //  Функция перевода типа
+    translateType: function (type) {
+      switch (type) {
+        case 'palace':
+          return 'Дворец';
+        case 'flat':
+          return 'Квартира';
+        case 'bungalo':
+          return 'Бунгало';
+        case 'house':
+          return 'Дом';
+        default:
+          return type;
+      }
+    },
 
-		//  Функция перевода типа
-		translateType: function (type) {
-		  switch (type) {
-		    case 'palace':
-		      return 'Дворец';
-		    case 'flat':
-		      return 'Квартира';
-		    case 'bungalo':
-		      return 'Бунгало';
-		    case 'house':
-		      return 'Дом';
-		    default:
-		      return type;
-		  }
-		},
+    // Функция генерации случайного массива
+    generateRandomArray: function (array) {
+      var randomArray = [];
+      randomArray = array.slice(0);
+      window.util.sortRandomArray(randomArray);
 
-		// Функция генерации случайного массива
-		generateRandomArray: function (array) {
-		  var randomArray = [];
-		  randomArray = array.slice(0);
-		  window.util.sortRandomArray(randomArray);
+      randomArray.length = window.util.getRandomNumber(0, randomArray.length + 1);
 
-		  randomArray.length = window.util.getRandomNumber(0, randomArray.length + 1);
-
-		  return randomArray;
-		}
-	};
+      return randomArray;
+    }
+  };
 })();
 
